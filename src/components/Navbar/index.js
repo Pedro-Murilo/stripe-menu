d37f924685104a52db22c330485274d5a8777b95
Pedro Murilo <pedromurilocalvi@gmail.com>
2021-03-08 19:14:23 -0300

    feat: created hook useDimension to calculate dimensions

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,29 +1,28 @@
-import React from "react";
-import { DropdownOption } from "../Dropdown";
+import { useContext } from "react";
+import { DropdownOption, DropdownProvider } from "../Dropdown";
 
 import { Container, DropdownStyles } from "./styles";
-import { Products, Developers, Company } from '../Content'
+import { Products, Developers, Company } from "../Content";
 
 function Navbar() {
   return (
-    <DropdownStyles>
-      <Container>
-        <ul>
-          <li>
-            <DropdownOption name="Products" content={Products} />
-          </li>
-          <li>
-            <DropdownOption
-              name="Developers"
-              content={Developers}
-            />
-          </li>
-          <li>
-            <DropdownOption name="Company" content={Company} />
-          </li>
-        </ul>
-      </Container>
-    </DropdownStyles>
+    <DropdownProvider>
+      <DropdownStyles>
+        <Container>
+          <ul>
+            <li>
+              <DropdownOption name="Products" content={Products} />
+            </li>
+            <li>
+              <DropdownOption name="Developers" content={Developers} />
+            </li>
+            <li>
+              <DropdownOption name="Company" content={Company} />
+            </li>
+          </ul>
+        </Container>
+      </DropdownStyles>
+    </DropdownProvider>
   );
 }
 
